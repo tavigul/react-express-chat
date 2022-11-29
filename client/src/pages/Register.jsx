1862e3d5d5998745a965be813ab288d067db24af
menashe_ta <menashe_ta@groupbwt.com>
2022-11-29 17:06:04 +0600

    (fix): change register styles. Add tailwindcss

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../assets/icons/logo.png";
 
 function Register() {
   const handleChange = (e) => {};
@@ -10,75 +9,55 @@ function Register() {
     console.log("submit!");
   };
   return (
-    <div>
+    <div className="register flex h-screen items-center justify-center">
       <form
+        className="form flex flex-col"
         onSubmit={(event) => {
           handleSubmit(event);
         }}
       >
-        <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-          <p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
-          <p className="text-gray-500 text-lg">
-            React and Tailwind CSS in action
-          </p>
-        </div>
-
-        <div>
-          <img src={Logo} alt="logo" />
-          <h1>Hello!</h1>
+        <div className="flex flex-col items-center justify-center">
+          <h1 class="text-center text-xl text-slate-50	">Sign up form</h1>
         </div>
 
         <input
           type="text"
+          className="my-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           placeholder="Username"
           name="username"
           onChange={(e) => handleChange(e)}
         />
-
         <input
           type="email"
+          className="my-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           placeholder="Email"
           name="email"
           onChange={(e) => handleChange(e)}
         />
-
         <input
           type="password"
-          placeholder="Enter password"
+          className="my-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          placeholder="Enter a password"
           name="password"
           onChange={(e) => handleChange(e)}
         />
-
         <input
           type="password"
+          className="my-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           placeholder="Confirm password"
           name="confirmPassword"
           onChange={(e) => handleChange(e)}
         />
-        <button type="submit">Register</button>
-        <span>
-          Already have an account? <Link to="login">Login</Link>
+
+        <button className="button" type="submit">
+          Register
+        </button>
+        <span className="register-link my-2  text-center">
+          Already have an account? <Link to="login"></Link>
         </span>
       </form>
     </div>
   );
 }
-
-// const FormContainer = styled.div`
-//   width: 100vw;
-//   height: 100vh;
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   align-items: center;
-//   gap: 1rem;
-//   background-color: #282E33;
-// `;
-
-// const LogoImg = styled.img`
-//   width: 30px
-// `
-
-// const Brand = styled.div``;
 
 export default Register;
